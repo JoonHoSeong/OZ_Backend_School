a6f5ed8b63e8f89468c26d99be4bed3d7bd973e9
joonhoseong <joonho1366@gmail.com>
2024-03-27 22:55:42 +0900

    add Readme

--- JavaScript/mini_project/dog_info_page/js/breeds.js
+++ JavaScript/mini_project/dog_info_page/js/breeds.js
@@ -1,108 +1,219 @@
-const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42"
-const apiAllBreeds = "https://dog.ceo/api/breeds/list/all"
-const request1 = new XMLHttpRequest()
-const request2 = new XMLHttpRequest()
+// const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42"
+// const apiAllBreeds = "https://dog.ceo/api/breeds/list/all"
+// const request1 = new XMLHttpRequest()
+// const request2 = new XMLHttpRequest()
 
-const header = document.getElementById("header")
-const main = document.getElementById("main")
-const input = document.getElementById("filter-text")
-const button = document.getElementById("filter-button")
-const select = document.getElementById("filter-select")
-const more = document.getElementById("more")
-const tothetop = document.getElementById("tothetop")
-const resetButton = document.getElementById("reset")
+// const header = document.getElementById("header")
+// const main = document.getElementById("main")
+// const input = document.getElementById("filter-text")
+// const button = document.getElementById("filter-button")
+// const select = document.getElementById("filter-select")
+// const more = document.getElementById("more")
+// const tothetop = document.getElementById("tothetop")
+// const resetButton = document.getElementById("reset")
 
-let currentDogs = []
+// let currentDogs = []
 
-function displayDogs(item){
-  const dogImgDiv = document.createElement("div")
-  dogImgDiv.classList.add("flex-item")
+// function displayDogs(item){
+//   const dogImgDiv = document.createElement("div")
+//   dogImgDiv.classList.add("flex-item")
+//   dogImgDiv.innerHTML = `
+//     <img src=${item}>
+//   `
+//   main.appendChild(dogImgDiv)
+// }
+
+
+// window.addEventListener("load", function(){
+
+//   // 강아지 사진 뿌리기
+//   request1.open("GET", apiRandomDogs)
+//   request1.addEventListener("load", function(){
+//     const response = JSON.parse(request1.response)
+//     response.message.forEach(function(item){
+//       currentDogs.push(item)
+//       displayDogs(item)
+//     });
+//   })
+//   request1.send()
+
+//   // 견종 텍스트 뿌리기
+//   request2.open("GET", apiAllBreeds)
+//   request2.addEventListener("load", function(){
+//     const response = JSON.parse(request2.response)
+//     Object.keys(response.message).forEach(function(item){
+//       const option = document.createElement("option")
+//       option.textContent = item
+//       option.value = item
+//       select.appendChild(option)
+//     });
+//   })
+//   request2.send()
+// })
+
+// button.addEventListener("click", function(){
+//   main.innerHTML = ""
+//   let filteredDogs = currentDogs.filter(function(item){
+//     return item.indexOf(input.value) !== -1
+//   })
+//   input.value = ""
+//   filteredDogs.forEach(function(item){
+//     displayDogs(item)
+//   });
+// })
+
+// select.addEventListener("change", function(){
+//   main.innerHTML = ""
+//   let filteredDogs = currentDogs.filter(function(item){
+//     return item.indexOf(select.value) !== -1
+//   })
+  
+//   filteredDogs.forEach(function(item){
+//     displayDogs(item)
+//   });
+// })
+
+// more.addEventListener("click", function(){
+//   // 강아지 사진 더 불러와서 추가하고 뿌리기
+//   request1.open("GET", apiRandomDogs)
+//   request1.addEventListener("load", function(){
+//     const response = JSON.parse(request1.response)
+//     response.message.forEach(function(item){
+//       currentDogs.push(item)
+//       displayDogs(item)
+//     });
+//   })
+//   request1.send()
+// })
+
+// tothetop.addEventListener("click", function(){
+//   window.scrollTo({ top: 0 })
+// })
+
+// //리셋버튼
+// resetButton.addEventListener('click', function(){
+
+
+//   request1.open("GET", apiRandomDogs)
+//   request1.addEventListener("load", function(){
+//     main.innerHTML = ""
+//     currentDogs = []
+//     const response = JSON.parse(request1.response)
+//     console.log(response)
+//     response.message.forEach(function(item){
+//       currentDogs.push(item)
+//       displayDogs(item)
+//     });
+//   })
+//   request1.send()
+// })
+
+
+const apiRandomDogs = "https://dog.ceo/api/breeds/image/random/42";
+const apiAllBreeds = "https://dog.ceo/api/breeds/list/all";
+const request1 = new XMLHttpRequest();
+const request2 = new XMLHttpRequest();
+
+const header = document.getElementById("header");
+const main = document.getElementById("main");
+const input = document.getElementById("filter-text");
+const button = document.getElementById("filter-button");
+const select = document.getElementById("filter-select");
+const more = document.getElementById("more");
+const tothetop = document.getElementById("tothetop");
+const resetButton = document.getElementById("reset");
+
+let currentDogs = [];
+
+function displayDogs(item) {
+  const dogImgDiv = document.createElement("div");
+  dogImgDiv.classList.add("flex-item");
   dogImgDiv.innerHTML = `
     <img src=${item}>
-  `
-  main.appendChild(dogImgDiv)
+  `;
+  main.appendChild(dogImgDiv);
 }
 
+function loadHandler() {
+  const response = JSON.parse(request1.response);
+  response.message.forEach(function (item) {
+    currentDogs.push(item);
+    displayDogs(item);
+  });
+}
 
-window.addEventListener("load", function(){
-
+window.addEventListener("load", function () {
   // 강아지 사진 뿌리기
-  request1.open("GET", apiRandomDogs)
-  request1.addEventListener("load", function(){
-    const response = JSON.parse(request1.response)
-    response.message.forEach(function(item){
-      currentDogs.push(item)
-      displayDogs(item)
-    });
-  })
-  request1.send()
+  request1.open("GET", apiRandomDogs);
+  request1.addEventListener("load", loadHandler);
+  // request1.addEventListener("load", function () {
+  //   const response = JSON.parse(request1.response);
+  //   response.message.forEach(function (item) {
+  //     currentDogs.push(item);
+  //     displayDogs(item);
+  //   });
+  // });
+  request1.send();
 
   // 견종 텍스트 뿌리기
-  request2.open("GET", apiAllBreeds)
-  request2.addEventListener("load", function(){
-    const response = JSON.parse(request2.response)
-    Object.keys(response.message).forEach(function(item){
-      const option = document.createElement("option")
-      option.textContent = item
-      option.value = item
-      select.appendChild(option)
+  request2.open("GET", apiAllBreeds);
+  request2.addEventListener("load", function () {
+    const response = JSON.parse(request2.response);
+    Object.keys(response.message).forEach(function (item) {
+      const option = document.createElement("option");
+      option.textContent = item;
+      option.value = item;
+      select.appendChild(option);
     });
-  })
-  request2.send()
-})
-
-button.addEventListener("click", function(){
-  main.innerHTML = ""
-  let filteredDogs = currentDogs.filter(function(item){
-    return item.indexOf(input.value) !== -1
-  })
-  input.value = ""
-  filteredDogs.forEach(function(item){
-    displayDogs(item)
   });
-})
+  request2.send();
+});
 
-select.addEventListener("change", function(){
-  main.innerHTML = ""
-  let filteredDogs = currentDogs.filter(function(item){
-    return item.indexOf(select.value) !== -1
-  })
-  
-  filteredDogs.forEach(function(item){
-    displayDogs(item)
+button.addEventListener("click", function () {
+  main.innerHTML = "";
+  let filteredDogs = currentDogs.filter(function (item) {
+    return item.indexOf(input.value) !== -1;
   });
-})
+  input.value = "";
+  filteredDogs.forEach(function (item) {
+    displayDogs(item);
+  });
+});
 
-more.addEventListener("click", function(){
+select.addEventListener("change", function () {
+  main.innerHTML = "";
+  let filteredDogs = currentDogs.filter(function (item) {
+    return item.indexOf(select.value) !== -1;
+  });
+
+  filteredDogs.forEach(function (item) {
+    displayDogs(item);
+  });
+});
+
+more.addEventListener("click", function () {
   // 강아지 사진 더 불러와서 추가하고 뿌리기
-  request1.open("GET", apiRandomDogs)
-  request1.addEventListener("load", function(){
-    const response = JSON.parse(request1.response)
-    response.message.forEach(function(item){
-      currentDogs.push(item)
-      displayDogs(item)
+  request1.open("GET", apiRandomDogs);
+  request1.addEventListener("load", function () {
+    const response = JSON.parse(request1.response);
+    response.message.forEach(function (item) {
+      currentDogs.push(item);
+      displayDogs(item);
     });
-  })
-  request1.send()
-})
+  });
+  request1.send();
+});
 
-tothetop.addEventListener("click", function(){
-  window.scrollTo({ top: 0 })
-})
+tothetop.addEventListener("click", function () {
+  window.scrollTo({ top: 0 });
+});
 
 //리셋버튼
-resetButton.addEventListener('click', function(){
-
-
-  request1.open("GET", apiRandomDogs)
-  request1.addEventListener("load", function(){
-    main.innerHTML = ""
-    currentDogs = []
-    const response = JSON.parse(request1.response)
-    response.message.forEach(function(item){
-      currentDogs.push(item)
-      displayDogs(item)
-    });
-  })
-  request1.send()
-})
+resetButton.addEventListener("click", function () {
+  main.innerHTML = "";
+  currentDogs = [];
+  request1.removeEventListener("load", loadHandler);
+  request1.addEventListener("load", loadHandler);
+  request1.open("GET", apiRandomDogs);
+  request1.send();
+});
---
+++ Python/README.md
@@ -0,0 +1,24 @@
+# OZ_Backend_School
+[오즈코딩스쿨](https://ozcodingschool.com/)의 'IT 스타트업 실무형 백엔드 개발 부트캠프'에서 작성한 실습 내용을 저장 공유 합니다!
+
+## 💬사용 기술
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+## 🧑‍💻과제
+### 🔥[챌린지](https://github.com/JoonHoSeong/OZ_Backend_School/tree/main/Python/homework)
+| 순서 | 과제 명 | 작성 일자  | 작성 언어  | 
+| --- | --- | --- | --- |
+| 1 | []() | 24.03.2. | ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) |
+
+### 🎆[Mini-Project](https://github.com/JoonHoSeong/OZ_Backend_School/tree/main/Python/mini_project)
+| 순서 | 과제 명 | 작성 일자 | 작성 언어  | 
+| --- | --- | --- | --- |
+| 1 | []() | 24.03.2. | ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) |
+
+<hr>
+
+## 🏃강의 실습
+### [Python](https://github.com/JoonHoSeong/OZ_Backend_School/tree/main/Python/Trainning)
+| 순서 | 실습 명 | 작성 일자 | 작성 언어  | 
+| --- | --- | --- | --- |
+| 1 | []() | 24.03.2. | ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) |
